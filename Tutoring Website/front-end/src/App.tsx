38939b91777bc6b04f4ac55983b6cd87bffc073e
Andy De Luna <raidenshogun05@gmail.com>
2025-01-29 23:53:16 -0600

    Add connected backend and front-end.
    Added the Django backend with the React front-end, as well as the connection of both.

---
+++ Tutoring Website/front-end/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import HelloWorld from './HelloWorld';
+
+function App() {
+  return (
+    <>
+      <HelloWorld />
+        <h1>Front-end!!!</h1>
+    </>
+  )
+}
+
+export default App

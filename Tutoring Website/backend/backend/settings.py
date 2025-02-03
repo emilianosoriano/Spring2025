3e6732568c4fb7d5f7313c6fcf03866c21cc82b6
Andy De Luna <raidenshogun05@gmail.com>
2025-02-02 21:05:09 -0600

    Fix secret_key
    Fixed an issue where the secret key was exposed

--- Tutoring Website/backend/backend/settings.py
+++ Tutoring Website/backend/backend/settings.py
@@ -11,16 +11,16 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-up&q&u$!c4duo$owtk-0=46qd_pfko8$j4&4+)^p*t^5585t)h"
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

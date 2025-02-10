f035004ac6852c6d6570ef4feaa37976652edc0e
Andy De Luna <raidenshogun05@gmail.com>
2025-02-09 20:16:05 -0600

    Change front-end to use next.js

--- Tutoring Website/backend/myapi/views.py
+++ Tutoring Website/backend/myapi/views.py
@@ -6,10 +6,21 @@ from .models import Lesson, Submission
 from rest_framework import status
 from django.http import JsonResponse
 
+lessons_data = [
+    {"id": 1, "title": "Variables and Data Types", "description": "Learn about variables."},
+    {"id": 2, "title": "Control Structures", "description": "Understand control flow."},
+    {"id": 3, "title": "Functions and Methods", "description": "Master functions."},
+    {"id": 4, "title": "Object-Oriented Programming", "description": "Dive into OOP."},
+]
 
 @api_view(['GET'])
 def hello_world(request):
     return Response({'message': 'Hello, world, Backend!!!'})
+
+# @api_view(['GET'])
+# def get_lessons(request, slug):
+#
+
 
 @api_view(['POST'])
 def submit_code(request):
